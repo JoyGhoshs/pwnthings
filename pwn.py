0f1747701690e22728a728afbcc25ceacb56b99c
Joy Ghosh <36255129+JoyGhoshs@users.noreply.github.com>
2021-08-13 06:25:53 -0400

    Create pwn.py

---
+++ pwn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from colorama import Fore, Style
+print('''
+{Fore.BLUE}
+
+█▀ █▄█ █▀ ▀█▀ █▀▀ █▀▄▀█ █▀█ █▀█ ▄▄ █▀ █▀▀ █▀▀ █░█ █▀█ █ ▀█▀ █▄█
+▄█ ░█░ ▄█ ░█░ ██▄ █░▀░█ █▄█ █▄█ ░░ ▄█ ██▄ █▄▄ █▄█ █▀▄ █ ░█░ ░█░{Fore.RED}
+
+                        U N D E R G R O U N D{Fore.WHITE}
+
+{Fore.RED}[X]{Fore.WHITE} Project is {Fore.GREEN}   Verified (✔){Fore.WHITE}
+{Fore.RED}[X]{Fore.WHITE} UnPublished
+''')
